e5866afc385d23686439caf4eb79363296844612
bouzagui <badrbouzagui@gmail.com>
2024-10-02 23:36:20 +0100

    done ✅

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+export default function uploadPhoto(filename) {
+    return new Promise((reject) => {
+        if (filename === 'guillaume.jpg') {
+            reject(new Error(`${filename} canot be processed`));
+        }
+    });
+}
