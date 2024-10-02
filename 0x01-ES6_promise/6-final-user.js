f233120a16d0af3fd28bef8f34fe68ef39650658
bouzagui <badrbouzagui@gmail.com>
2024-10-03 00:14:43 +0100

    done ✅

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    let promises = ([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    let result = [];
+    return Promise.allSettled(promises).then((res) => {
+        res.map(({ status, value, reason }) => {
+          result.push({
+            status,
+            value: status === 'rejected' ? reason.toString() : value.body,
+          })
+    });
+    return result;
+    });
+}
