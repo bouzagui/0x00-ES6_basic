0e664f368cbc541d9c3c00e3f9525a85cabc8fae
bouzagui <badrbouzagui@gmail.com>
2024-10-07 22:27:47 +0100

    done ✅

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+export default function getListStudents() {
+  const list = [
+    {
+      id: 1, firstName: 'Guillaume', location: 'San Francisco',
+    },
+    {
+      id: 2, firstName: 'James', location: 'Columbia',
+    },
+    {
+      id: 5, firstName: 'Serena', location: 'San Francisco',
+    },
+  ];
+  return list;
+}
